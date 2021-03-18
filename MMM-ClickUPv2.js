8478ebe8ffeb66ab289ef11255826da723846c2c
Andrew Matayka <andrewmatayka2001@gmail.com>
2021-03-18 17:59:13 -0500

    Updated to actually work and use subtasks. Lots of changes.

--- MMM-ClickUPv2.js
+++ MMM-ClickUPv2.js
@@ -3,112 +3,481 @@
  * Module: MMM-ClickUPv2
  * Version: 0.01
  */
-
-Module.register("MMM-ClickUPv2", {
-    //Default Config Values
-    defaults: {
-        updateInterval: 1 * 1 * 1000, //How often module is updated: (Min * Sec * Ms) = Every 60 Seconds
-
-        clientID: "CZ2CGOL98DNVSMP4O4T3YN26L7E4E4VR",
-        clientSecret: "2YFBQZPOZ47OZGEAORT8LYFHX6ICILIJD8CIPZFBMMIY8O48QL29ZDAJ6REN4E12",
-        accessCode: "C22YB0MRTDM6R1M80DCNI4JNRDRFLUEN",
-
-        accessToken: "", //Access Token required for authorization of API
-    },
-
-    //Gets the styles for the module
-    getStyles: function () {
-        return [
-            this.file('MMM-ClickUPv2.css')
-        ]
-    },
-
-    //The header displayed above the Module - Can be configured by User
-    getHeader: function () {
-        if (this.data.header === undefined) {
-            return 'ClickUP';
-        } else {
-            return this.data.header;
-        }
-    },
-
-    //Ran at start of module
-    start: function () {
-        Log.trace("Getting Styles: " + this.getStyles(), this);
-        Log.trace("Starting Module", this);
-        var self = this; //Set variable self to this object
-
-        //Need to check if we already have Access Token. If not we need to acquire it.
-        if (this.config.accessToken !== null && this.config.accessToken !== undefined && this.config.accessToken !== "") {
-            //We have Access Token
-            Log.trace("Found Access Token: " + this.config.accessToken, this)
-        } else {
-            //Send request to get Access Token
-
-            Log.trace("Requesting Access Token", this)
-            this.sendSocketNotification('Request_AccessToken', this.config); //Request Access Token from Node Helper
-        }
-
-        setInterval(function () {
-            self.updateDom(); // no speed defined, so it updates instantly. Updates the Dom of the page
-        }, this.config.updateInterval); //perform every update interval 
-    },
-
-    //Ran on suspension of module
-    suspend: function () {
-
-    },
-
-    //Ran on resume of module
-    resume: function () {
-
-    },
-
-    //Ran when receiving notification from System or other Modules
-    notificationReceived: function (notification, payload, sender) {
-
-    },
-
-    //Ran when receiving notification from Node Helper
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "AccessToken") {
-            Log.trace("Received Access Token: " + payload, this);
-        } else if (notification === "Error") {
-            Log.error("Received Error: " + JSON.parse(payload.error).err, this)
-            alert("Error: " + JSON.parse(payload.error).err);
-        }
-    },
-
-    //Ran when updating HTML of Module
-    getDom: function () {
-        var wrapper = document.createElement('div');
-
-        Log.trace("GetDom Called", this);
-        wrapper.innerHTML = "Loading...";
-
-        return wrapper;
-    }
-});
+let receivedToken = false;
+let loaded = false;
 
 //NEW LOG FUNCTION
-var Log = {
-    info: function (message) {
-        console.log("%cInfo: " + message + "\n", "color: gray");
-    },
+const Log = {
+	info: function (message) {
+		console.log("%cInfo: " + message + "\n", "color: gray");
+	},
 
-    trace: function (message, that) {
-        console.log("%c" + that.name + ": " + message, "color: cyan");
-    },
+	trace: function (message, that) {
+		console.log("%c" + that.name + ": " + message, "color: cyan");
+	},
 
-    log: function (message) {
-        console.log(message);
-    },
+	log: function (message) {
+		console.log(message);
+	},
 
-    error: function (message) {
-        console.log("%cError: " + message, "color: red");
-    },
+	error: function (message) {
+		console.log("%cError: " + message, "color: red");
+	},
 
-    error: function (message, that) {
-        console.log("%c" + that.name + " - Error: " + message, "color: red");
-    }
-}
+	error: function (message, that) {
+		console.log("%c" + that.name + " - Error: " + message, "color: red");
+	},
+
+	table: function (message) {
+		console.table(message);
+	}
+};
+
+Module.register("MMM-ClickUPv2", {
+	//Default Config Values
+	defaults: {
+		updateInterval: 60 * 1000, //How often module is updated: (Min * Sec * Ms) = Every 60 Seconds
+
+		taskTypes: ["Class", "Type"], //The Task Types you want to be shown (Custom Fields)
+		highlightedTaskTypes: ["Class"], //The highlighted Task Type for Custom Fields
+		completedStatus: "submitted", //Name of the status used to mark completion
+		maxCompletedTaskCount: 3, //The max amount of shown completed tasks
+
+		clientID: "CZ2CGOL98DNVSMP4O4T3YN26L7E4E4VR",
+		clientSecret: "2YFBQZPOZ47OZGEAORT8LYFHX6ICILIJD8CIPZFBMMIY8O48QL29ZDAJ6REN4E12",
+		accessCode: "40R1Z3TX3EYQ9HW1VSZBMM1EMX3MJNH2",
+
+		accessToken: "", //Access Token required for authorization of API
+
+		debug: false,
+	},
+
+	//Gets the styles for the module
+	getStyles: function () {
+		return [
+			this.file('MMM-ClickUPv2.css')
+		]
+	},
+
+	//The header displayed above the Module - Can be configured by User
+	getHeader: function () {
+		if (this.data.header === undefined) {
+			return 'ClickUP';
+		} else {
+			return this.data.header;
+		}
+	},
+
+	//Ran at start of module
+	start: function () {
+		if (this.config.debug) {
+			Log.trace("Getting Styles: " + this.getStyles(), this);
+		}
+
+		Log.trace("Starting Module", this);
+		const self = this; //Set variable self to this object
+
+		//Need to check if we already have Access Token. If not we need to acquire it.
+		if (this.config.accessToken !== null && this.config.accessToken !== undefined && this.config.accessToken !== "") {
+			//We have Access Token
+			if (this.config.debug) Log.trace("Found Access Token: " + this.config.accessToken, this);
+		} else {
+			//Send request to get Access Token
+			if (this.config.debug) Log.trace("Requesting Access Token", this);
+			this.sendSocketNotification('Request_AccessToken', this.config); //Request Access Token from Node Helper
+		}
+
+		let interId = setInterval(() => {
+			if (receivedToken !== true && (this.config.accessToken === "" || this.config.accessToken === null || this.config.accessToken === undefined)) {
+				loaded = false;
+				if (this.config.debug) Log.trace("Access Token Not Available Yet", this);
+			} else {
+				loaded = true;
+				if (this.config.debug) Log.trace("Access Token Available!", this);
+				this.continueStart();
+				clearInterval(interId);
+			}
+		}, 50);
+	},
+
+	//Ran when Access Token is found and we can continue startup
+	continueStart: function () {
+		const self = this;
+
+		//Run Code Here to Fetch first tasks list, then interval to keep requesting tasks list.
+		self.sendSocketNotification('Request_TasksList', this.config); //Request Access Token from Node Helper
+
+		setInterval(function () {
+			self.sendSocketNotification('Request_TasksList', this.config); //Request Access Token from Node Helper
+		}, this.config.updateInterval);
+	},
+
+	//Ran on suspension of module
+	suspend: function () {
+
+	},
+
+	//Ran on resume of module
+	resume: function () {
+
+	},
+
+	//Ran when receiving notification from System or other Modules
+	notificationReceived: function (notification, payload, sender) {
+
+	},
+
+	//Ran when receiving notification from Node Helper
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "AccessToken") {
+			if (this.config.debug) Log.trace("Received Access Token: " + payload, this);
+			this.config.accessToken = payload;
+			receivedToken = true;
+		} else if (notification === "Tasks_List") {
+			if (this.config.debug) Log.trace("Received Tasks List: " + payload, this);
+
+			this.filterTasksList(payload.tasks);
+		} else if (notification === "Error") {
+			Log.error("Received Error: " + JSON.parse(payload.error).err, this)
+		}
+	},
+
+	filterTasksList: function (tasks) {
+		let submittedCount = 0;
+
+		//Reset the list each run to not make duplicates
+		if (this.tasks !== undefined) {
+			if (this.tasks.items.length > 0 || this.tasks.parents.length > 0) {
+				this.tasks.items.length = 0;
+				this.tasks.parents.length = 0;
+			}
+		}
+
+		const self = this; //Setting self so we have a localized instance
+		let items = []; //Array of individual tasks that dont have parents
+		let parents = []; //Array of individual tasks that have parents
+
+		//Log tasks to a table if debug is enabled
+		if (this.config.debug) Log.table(tasks);
+
+		//Push every task into the array of items so we can access tasks individually.
+		for (let i = 0; i < tasks.length; i++) {
+			if (tasks[i].parent === null || tasks[i].parent === "" || tasks[i].parent === undefined) {
+				if (tasks[i].status.status !== this.config.completedStatus || submittedCount < this.config.maxCompletedTaskCount) {
+					items.push(tasks[i]);
+					if (tasks[i].status.status === this.config.completedStatus) submittedCount++;
+				}
+			} else {
+				parents.push(tasks[i]);
+			}
+		}
+
+		this.tasks = {
+			"items": items,
+			"parents": parents
+		}
+
+		self.updateDom();
+	},
+
+	//#region CreateCellTypes
+	createCell: function (className, innerHTML) {
+		let cell = document.createElement("td");
+		cell.className = "divTableCell " + className;
+		cell.innerHTML = innerHTML;
+
+		if (cell.className.includes("title")) {
+			if (innerHTML.includes("-->")) {
+				cell.colSpan = 1;
+			} else {
+				cell.colSpan = 2;
+			}
+		}
+
+		return cell;
+	},
+
+	//Add suffix to date depending on the day. {st, nd, rd, th}
+	dateOrdinal: function (d) {
+		return d + (31 === d || 21 === d || 1 === d ? "st" : 22 === d || 2 === d ? "nd" : 23 === d || 3 === d ? "rd" : "th");
+	},
+
+	//Add Due Date Cell and Localize Names based on how far they are (Monday, Tuesday, Tomorrow, Today, Yesterday, Etc...)
+	addDueDateCell: function (item) {
+		let className = "align-right dueDate ";
+		let innerHTML = "";
+
+		if (item.due_date !== null) {
+			className += "bright ";
+			const oneDay = 24 * 60 * 60 * 1000;
+			let dueDateTime = new Date(parseInt(item.due_date));
+			let dueDate = new Date(dueDateTime.getFullYear(), dueDateTime.getMonth(), dueDateTime.getDate());
+			let now = new Date();
+			let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+			let diffDays = Math.floor((dueDate - today) / (oneDay));
+			let diffMonths = (dueDate.getFullYear() * 12 + dueDate.getMonth()) - (now.getFullYear() * 12 + now.getMonth());
+			let allDay = (dueDateTime.getHours() + ":" + dueDateTime.getMinutes());
+
+			if (diffDays < -1) {
+				innerHTML = dueDate.toLocaleDateString(config.language, {
+					"month": "long"
+				}) + " " + this.dateOrdinal(dueDate.getDate());
+				className += "xsmall overdue";
+			} else if (diffDays === -1) {
+				innerHTML = this.translate("YESTERDAY");
+				className += "xsmall overdue";
+			} else if (diffDays === 0) {
+				innerHTML = this.translate("TODAY");
+				if (allDay === "4:0" || dueDateTime >= now) {
+					className += "today";
+				} else {
+					className += "overdue";
+				}
+			} else if (diffDays === 1) {
+				innerHTML = this.translate("TOMORROW");
+				className += "xsmall tomorrow";
+			} else if (diffDays < 7) {
+				innerHTML = dueDate.toLocaleDateString(config.language, {
+					"weekday": "long"
+				});
+				className += "xsmall";
+			} else if (diffMonths < 7 || dueDate.getFullYear() === now.getFullYear()) {
+				innerHTML = dueDate.toLocaleDateString(config.language, {
+					"month": "long"
+				}) + " " + this.dateOrdinal(dueDate.getDate());
+				className += "xsmall";
+			} else if (item.due_date === "2100-12-31") {
+				innerHTML = "";
+				className += "xsmall";
+			} else {
+				innerHTML = dueDate.toLocaleDateString(config.language, {
+					"month": "long"
+				}) + " " + this.dateOrdinal(dueDate.getDate()) + " " + dueDate.getFullYear();
+				className += "xsmall";
+			}
+
+			if (innerHTML !== "" && allDay !== "4:0") {
+				function formatTime(d) {
+					function z(n) {
+						return (n < 10 ? "0" : "") + n;
+					}
+
+					let h = d.getHours();
+					let m = z(d.getMinutes());
+					if (config.timeFormat === 12) {
+						return " " + (h % 12 || 12) + ":" + m + (h < 12 ? " AM" : " PM");
+					} else {
+						return " " + h + ":" + m;
+					}
+				}
+
+				innerHTML += " @ " + formatTime(dueDateTime);
+			}
+		} else {
+			innerHTML += "N/A";
+			className += "light xsmall normal";
+		}
+		return this.createCell(className, innerHTML);
+	},
+
+	//Add Title Cell
+	addTitleCell: function (item) {
+		//If subtask then add subtask arrow
+		if (item.parent !== null) {
+			item.name = "--> " + item.name;
+		}
+
+		return this.createCell("title bright alignLeft", item.name);
+	},
+
+	//Add Space for Organization
+	addColumnSpacerCell: function () {
+		return this.createCell("spacer", "&nbsp;");
+	},
+
+	//Add Space for Organization and Subtasks
+	addColumnSubSpacerCell: function () {
+		return this.createCell("subSpacer", "&nbsp;");
+	},
+
+	//Add Priority indicator for task
+	addPriorityIndicatorCell: function (item) {
+		let className = "priority ";
+		if (item.priority !== null) {
+			switch (parseInt(item.priority.id)) {
+				case 1:
+					className += "priority1";
+					break;
+				case 2:
+					className += "priority2";
+					break;
+				case 3:
+					className += "priority3";
+					break;
+				case 4:
+					className += "priority4";
+					break;
+				default:
+					className = "";
+					break;
+			}
+		} else {
+			className += "priority1";
+		}
+		return this.createCell(className, "&nbsp;");
+	},
+
+	//Add Type Cell depending on input. Uses Custom Field
+	addTypeCell: function (item, typeName, highlightedType) {
+		let typeValIndex = 0;
+		let typeValName = "N/A";
+		let typeValColor = null;
+
+		let count = 0;
+		item.custom_fields.forEach(field => {
+			if (field.name.toUpperCase() === typeName.toUpperCase()) {
+				typeValIndex = count;
+			}
+			count++;
+		});
+
+		if (item.custom_fields[typeValIndex].value !== undefined) {
+			typeValName = item.custom_fields[typeValIndex].type_config.options[item.custom_fields[typeValIndex].value].name;
+			typeValColor = item.custom_fields[typeValIndex].type_config.options[item.custom_fields[typeValIndex].value].color;
+		}
+
+		let innerHTML = "";
+		if (highlightedType === false) {
+			innerHTML = "<span style='color: " + typeValColor + ";'>" + typeValName + "</span>";
+		} else {
+			innerHTML = "<span class='projectcolor' style='color: " + typeValColor + "; background-color: " + typeValColor + "'></span>" + typeValName;
+		}
+
+		return this.createCell("xsmall", innerHTML);
+	},
+	////#endregion
+
+	//Ran when updating HTML of Module
+	getDom: function () {
+		let wrapper = document.createElement('div');
+
+		if (this.config.debug) Log.trace("Method GetDom Called", this);
+
+		//If not loaded return loading screen.
+		if (!loaded) {
+			wrapper.innerHTML = "Loading...";
+			return wrapper;
+		}
+
+		//Create Container containing the main table
+		let divTable = document.createElement("div");
+		divTable.className = "divTable normal small light";
+
+		//Create table that contains all of the Task Data
+		let divBody = document.createElement("table");
+		divBody.className = "divTableBody";
+
+		//If there are no tasks then return empty wrapper(HTML).
+		if (this.tasks === undefined) {
+			return wrapper;
+		}
+
+		//Insert subtasks below their parent tasks
+		this.tasks.parents.forEach(item => {
+			if (this.config.debug) Log.trace("Parent: " + item.name + " | " + item.parent + "<--(Parent's ID)", this);
+
+			let index = null, count = 0;
+
+			this.tasks.items.forEach(itemParents => {
+				if (itemParents.id === item.parent) {
+					index = count;
+				}
+				count++;
+			});
+
+			//If parent task is submitted, then subtasks shouldn't be added; If we can't find the index for the parent of our subtask, then we can ignore them.
+			if (index !== null) {
+				if (this.tasks.items[index].status.status !== this.config.completedStatus) {
+					this.tasks.items.splice(index + 1, 0, item)
+				}
+			}
+		});
+
+		this.tasks.items.forEach(item => {
+			if (this.config.debug) Log.trace("Item: " + item.name + " | " + item.id, this);
+
+			//Create Row holding individual Task Data
+			let divRow = document.createElement("tr");
+			divRow.className = "divTableRow";
+
+			//Check if task is submitted or not yet, if so strikethrough the task name.
+			if (item.status.status === this.config.completedStatus) {
+				divRow.className += " strikethrough"
+				divRow.style.filter = "brightness(50%) grayscale(100%)";
+			}
+
+			//Add Priority Cell
+			divRow.appendChild(this.addPriorityIndicatorCell(item));
+
+			//Add Status Indicator Cell
+			//divRow.appendChild(this.addStatusIndicatorCell(item, ""));
+
+			//Make Subtasks have extra spacer cell, to make them look like subtasks
+			divRow.appendChild(this.addColumnSpacerCell());
+			if (item.parent !== null) {
+				divRow.appendChild(this.addColumnSubSpacerCell());
+			}
+
+			//Add Title Cell
+			divRow.appendChild(this.addTitleCell(item));
+
+			//Add Due Date Cell
+			divRow.appendChild(this.addDueDateCell(item));
+
+			//Spacing
+			divRow.appendChild(this.addColumnSpacerCell());
+
+			//If debugging then show the possible task types.
+			if (this.config.debug) Log.trace("Possible Task Types: " + this.getPossibleTaskTypes(), this);
+
+			//Use Task Types from config in order to add the Type Columns you desire.
+			let taskTypes = Object.values(this.config.taskTypes);
+			let highlightedTaskTypes = Object.values(this.config.highlightedTaskTypes);
+
+			taskTypes.forEach(type => {
+				highlightedTaskTypes.forEach(high => {
+					if (type === high) {
+						divRow.appendChild(this.addTypeCell(item, type, true));
+					} else {
+						divRow.appendChild(this.addTypeCell(item, type, false));
+					}
+				})
+			});
+
+			//Make sure there are no duplicates being made
+			let arr = Array.from(divBody.children);
+			if (arr.includes(divRow)) return;
+
+			//Add the Row with the Individual Task Data to the Table
+			divBody.appendChild(divRow);
+		});
+
+		//Add the Table to the main HTML content
+		divTable.appendChild(divBody);
+		wrapper.appendChild(divTable);
+
+		//Return all of our HTML
+		return wrapper;
+	},
+
+	//Return the possible Task Types (Custom Fields)
+	getPossibleTaskTypes: function () {
+		let typesList = "";
+
+		this.tasks.items[0].custom_fields.forEach(field => {
+			typesList += field.name + ", ";
+		});
+
+		return typesList;
+	}
+});
--- node_helper.js
+++ node_helper.js
@@ -6,68 +6,192 @@
 
 const NodeHelper = require("node_helper");
 const request = require("request");
+const fs = require("fs");
+
+let accessToken = undefined;
 
 module.exports = NodeHelper.create({
-    //Ran on the start of the node helper
-    start: function () {
-        console.log("MMM-ClickUPv2: Node Helper Started");
-    },
+	//Ran on the start of the node helper
+	start: function () {
+		console.log("%cMMM-ClickUPv2 (node): Node Helper Started!", "color: yellow;");
+	},
 
-    //Ran when receiving notification from the module
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "Request_AccessToken") {
-            console.log("MMM-ClickUPv2: Request Access Token Notification Received");
+	//Ran when receiving notification from the module
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "Request_AccessToken") {
+			this.config = payload;
 
-            this.config = payload;
-            this.requestAccessToken();
-        }
-    },
+			if (this.config.debug) console.log("\n%cMMM-ClickUPv2 (node): Request Access Token Notification Received!", "color: yellow;");
 
-    requestAccessToken: function () {
-        var self = this;
+			this.requestAccessToken();
+		} else if (notification === "Request_TasksList") {
+			this.config = payload;
 
-        request({
-            url: "https://api.clickup.com/api/v2/oauth/token",
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                "cache-control": "no-cache"
-            },
-            form: {
-                client_id: self.config.clientID,
-                client_secret: self.config.clientSecret,
-                code: self.config.accessCode
-            }
-        },
-            function (error, response, body) {
-                if (error) {
-                    self.sendSocketNotification("Error", {
-                        error: error
-                    });
-                    return console.error("MMM-ClickUPv2: " + error);
-                }
-                //if(this.config.debug){
-                //	console.log(body);
-                //}
-                if (response.statusCode === 200) {
-                    //var responseJson = JSON.parse(body);
-                    //accessToken = responseJson.access_token;
-                    console.log("Access Token: " + accessToken);
+			if (this.config.debug) console.log("\n%cMMM-ClickUPv2 (node): Request Tasks List Notification Received!", "color: green;");
 
-                    //fs.writeFile('accessToken.txt', accessToken, function (err) {
-                    //    if (err) return console.log(err);
-                    //    console.log(accessToken + ' > accessToken.txt');
-                    //});
-                } else {
-                    console.log("ClickUP api request status=" + response.statusCode);
-                    console.log("RESPONSE: " + response.body);
+			this.requestTasks();
+		}
+	},
 
-                    self.sendSocketNotification("Error", {
-                        error: response.body,
-                        statusCode: response.statusCode
-                    });
-                }
+	requestAccessToken: function () {
+		const self = this;
 
-            });
-    },
+		const path = self.path;
+		let aTFData = "";
+
+		//Find out if AccessToken is already found and in file. If not find the Access Token
+		return new Promise(resolve => {
+			fs.readFile(path + '/accessToken', 'UTF-8', (error, data) => {
+				if (error) {
+					self.sendSocketNotification("Error", {
+						error: error
+					});
+					return console.error("MMM-ClickUPv2 (node): " + error);
+				}
+				aTFData = data;
+
+				//If the AccessToken file does not contain the Access Token, or does not exist.
+				if (aTFData.length === 0) {
+					if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Requesting Access Token!", "color: yellow;")
+
+					request({
+							url: "https://api.clickup.com/api/v2/oauth/token",
+							method: "POST",
+							headers: {
+								"content-type": "application/json",
+								"cache-control": "no-cache"
+							},
+							form: {
+								client_id: self.config.clientID,
+								client_secret: self.config.clientSecret,
+								code: self.config.accessCode
+							}
+						},
+						function (error, response, body) {
+							if (error) {
+								self.sendSocketNotification("Error", {
+									error: error
+								});
+								return console.error("MMM-ClickUPv2 (node): " + error);
+							}
+							if (response.statusCode === 200) {
+								let responseJson = JSON.parse(body);
+								accessToken = responseJson.access_token;
+								if (self.config.debug) console.log("%cMMM-ClickUPv2 (node): Access Token Received: '" + accessToken + "'!", "color: yellow;");
+
+								fs.appendFile(path + '/accessToken', accessToken, (err) => {
+									if (err) {
+										console.log(err)
+									}
+									if (self.config.debug) console.log("%cMMM-ClickUPv2 (node): Writing Access Token to file!", "color: yellow;")
+								});
+
+								resolve(accessToken);
+							} else {
+								if (self.config.debug) console.log("%cMMM-ClickUPv2 (node): ClickUP api request status - '" + response.statusCode + "'!", "color: yellow;");
+								if (self.config.debug) console.log("%cMMM-ClickUPv2 (node): ClickUP api request response - '" + response.body + "'!", "color: yellow;");
+
+								self.sendSocketNotification("Error", {
+									error: response.body,
+									statusCode: response.statusCode
+								});
+							}
+						});
+				} else { //If the Access Token is found in the accessToken file
+					if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Read Access Token from file: '" + aTFData + "'!", "color: yellow;");
+					resolve(aTFData);
+				}
+			});
+		}).then(value => {
+			accessToken = value;
+			self.config.accessToken = value;
+			self.sendSocketNotification("AccessToken", value);
+		});
+	},
+
+	requestTasks: async function () {
+		const self = this;
+
+		if (accessToken !== undefined) {
+			return new Promise(resolve => {
+				if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Requesting Teams ID!", "color: green;");
+				request({
+					url: "https://api.clickup.com/api/v2/team",
+					method: "GET",
+					headers: {
+						Authorization: accessToken
+					}
+				}, function (error, response, body) {
+					if (!error)
+						resolve(JSON.parse(body).teams[0].id);
+				});
+			}).then(value => {
+				if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Received Teams ID: " + value, "color: green;");
+
+				return new Promise(function (resolve) {
+					request({
+						url: 'https://api.clickup.com/api/v2/team/' + value + '/space',
+						method: "GET",
+						headers: {
+							Authorization: accessToken
+						},
+					}, function (error, response, body) {
+						if (!error)
+							resolve(JSON.parse(body).spaces[0].id);
+					});
+				}).then(value => {
+					if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Received Spaces ID: " + value, "color: green;");
+
+					return new Promise(function (resolve) {
+						request({
+							url: 'https://api.clickup.com/api/v2/space/' + value + '/folder',
+							method: "GET",
+							headers: {
+								Authorization: accessToken
+							},
+						}, function (error, response, body) {
+							if (!error)
+								resolve(JSON.parse(body).folders[1].id);
+						});
+					}).then(value => {
+						if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Received Folders ID: " + value, "color: green;")
+
+						return new Promise(function (resolve) {
+							request({
+								url: 'https://api.clickup.com/api/v2/folder/' + value + '/list',
+								method: "GET",
+								headers: {
+									Authorization: accessToken
+								},
+							}, function (error, response, body) {
+								if (!error)
+									resolve(JSON.parse(body).lists[0].id);
+							});
+						}).then(value => {
+							if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Received Lists ID: " + value, "color: green;");
+
+							return new Promise(function (resolve) {
+								request({
+									url: 'https://api.clickup.com/api/v2/list/' + value + '/task?archived=false&subtasks=true&include_closed=true&order_by=due_date',
+									method: "GET",
+									headers: {
+										Authorization: accessToken
+									},
+								}, function (error, response, body) {
+									if (!error)
+										resolve(JSON.parse(body));
+								});
+							}).then(value => {
+								if (this.config.debug) console.log("%cMMM-ClickUPv2 (node): Received Tasks:", "color: green;");
+
+								self.sendSocketNotification("Tasks_List", value);
+							});
+						});
+					});
+				});
+			})
+		} else {
+			if (this.config.debug) console.log("%cMMM-ClickUPv2: No Access Token Found!!!", "color: cyan;");
+		}
+	}
 });
